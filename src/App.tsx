58ff0249a50a24386f6bbb1fb0748520629f98de
nduaguba chiemezie anthony <anthonynduaguba123@gmail.com>
2025-07-16 02:19:22 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Navbar from './sections/Navbar.js'
-import Intro from './sections/Intro.js'
 import About from './sections/About.js'
 import Resume from './sections/resume/Resume.js'
 import Footer from './sections/Footer.js'
@@ -12,7 +11,6 @@ function App() {
     <div className='bg-gray-50 text-gray-800'>
       <Navbar />
       <div className='flex flex-col gap-20 px-5 md:px-10 lg:px-16 xl:px-32'>
-        {/* <Intro /> */}
         <About />
         <Resume />
         <Projects />
